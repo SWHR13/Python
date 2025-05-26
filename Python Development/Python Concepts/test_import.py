876818cfdcd6fe2f9eccb155e6bef1f7d5fbc6a1
SWHR13 <160714203+SWHR13@users.noreply.github.com>
2025-05-26 18:05:36 +0500

    Update test_import.py

--- Python Development/Python Concepts/test_import.py
+++ Python Development/Python Concepts/test_import.py
@@ -1,2 +1,24 @@
-import mymodule
-mymodule.greetings("Jonathan")
+# import mymodule
+# a = mymodule.person1["age"]
+# print(a)
+
+# Renaming a module
+from mymodule import person1
+import platform
+import mymodule as mx
+a = mx.person1["age"]
+print(a)
+
+# Built-in Modules
+# There are several built-in modules in Python, which you can import whenever you like.
+x = platform.system()
+print(x)
+
+"""Using the dir() Function
+There is a built-in function to list all the function names (or variable names) in a module. The dir() function:
+"""
+x = dir(platform)
+print(x)
+
+#from mymodule import person1
+print(person1["age"])
